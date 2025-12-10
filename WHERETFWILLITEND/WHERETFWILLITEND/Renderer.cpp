7f5805519524bcb378e58afe51d9caf1ee3f6363
maxkungurtsev <simkungurtsev@gmail.com>
2025-12-10 11:52:30 +0300

    ask descriptor sizes
    ima tired of making commits creatve

--- WHERETFWILLITEND/WHERETFWILLITEND/Renderer.cpp
+++ WHERETFWILLITEND/WHERETFWILLITEND/Renderer.cpp
@@ -1,4 +1,14 @@
 #include "Renderer.h"
-HRESULT Renderer::InitDevice() {
-    return D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device));
+HRESULT Renderer::CreateGraphicsDevice() {
+    return D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device_));
+
+};
+void Renderer::CreateFence() {
+    device_->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
+};
+void Renderer::AskDescryptorSizes() {
+    rtv_descriptor_size_ =device_->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    dsv_descriptor_size_ =device_->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
+    cbv_srv_uav_descriptor_size_ =device_->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+    sampler_descriptor_size_ =device_->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER);
 };
--- WHERETFWILLITEND/WHERETFWILLITEND/Renderer.h
+++ WHERETFWILLITEND/WHERETFWILLITEND/Renderer.h
@@ -8,16 +8,32 @@ using Microsoft::WRL::ComPtr;
 class Renderer
 {
 private:
-        Microsoft::WRL::ComPtr<ID3D12Device> device;
-        Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain;
-        Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue;
-        Microsoft::WRL::ComPtr<ID3D12CommandAllocator> commandAllocator;
-        Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> commandList;
+        Microsoft::WRL::ComPtr<ID3D12Device> device_;
+        Microsoft::WRL::ComPtr<IDXGISwapChain> swap_chain_;
+        Microsoft::WRL::ComPtr<ID3D12CommandQueue> command_queue_;
+        Microsoft::WRL::ComPtr<ID3D12CommandAllocator> command_allocator_;
+        Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> command_list_;
         Microsoft::WRL::ComPtr<ID3D12Fence> fence;
-        UINT64 fenceValue;
-        HANDLE fenceEvent;
-
+        ComPtr<ID3D12DescriptorHeap> rtv_heap_;
+        UINT rtv_descriptor_size_;
+        ComPtr<ID3D12DescriptorHeap> dsv_heap_;
+        UINT dsv_descriptor_size_;
+        ComPtr<ID3D12DescriptorHeap> cbv_srv_uav_heap_;
+        UINT cbv_srv_uav_descriptor_size_;
+        ComPtr<ID3D12DescriptorHeap> sampler_heap_;
+        UINT sampler_descriptor_size_;
 public:
-    HRESULT InitDevice();
-
+    // step2
+    HRESULT CreateGraphicsDevice();
+    // step3.1
+    void CreateFence();
+    // step3.2
+    void AskDescryptorSizes();
+    // step4
+    // step5
+    // step6
+    // step7
+    // step8
+    // step9
+    // step10
 };
