595598dfcdb1414efeafc257eaf3a367458f863f
maxkungurtsev <simkungurtsev@gmail.com>
2026-04-09 22:39:30 +0300

    octree architecture

--- WHERETFWILLITEND/WHERETFWILLITEND/OctreeNode.h
+++ WHERETFWILLITEND/WHERETFWILLITEND/OctreeNode.h
@@ -5,10 +5,11 @@ class OctreeNode {
 	std::unique_ptr<OctreeNode>children_[8];
 	std::vector<int> submesh_indices;
 	BoundingBox bounding_box_;
+	bool leaf_ = false;
 public:
 	OctreeNode(int current_depth, BoundingBox& box) {
 		bounding_box_ = box;
-		if (current_depth>0) {
+		if (current_depth > 0) {
 			for (int i = 0; i < 8; i++) {
 				XMFLOAT3 center = bounding_box_.Center;
 				XMFLOAT3 ext = bounding_box_.Extents;
@@ -18,13 +19,29 @@ public:
 				int z_offset = (i & 4) ? 1 : -1;
 				center = XMFLOAT3(center.x + x_offset * ext.x, center.y + y_offset * ext.y, center.z + z_offset * ext.z);
 				BoundingBox box(center, ext);
-				children_[i] = std::make_unique<OctreeNode>(current_depth-1, box);
+				children_[i] = std::make_unique<OctreeNode>(current_depth - 1, box);
 			}
 		}
-	}
-	void FillTree(SubMesh& sub_mesh_, int index) {
-		for (int i = 0; i < 8; i++) {
-			if (children_[i]->bounding_box_.Contains(sub_mesh_.bounding_sphere_)) {};
+		else {
+			leaf_ = true;
 		}
+	}
+	void AddSubmeshToTree(SubMesh& sub_mesh_, int index) {
+		bool in_child = false;
+		if (leaf_) {
+			submesh_indices.push_back(index);
+			return;
+		}
+		for (int i = 0; i < 8; i++) {
+			if (children_[i]->bounding_box_.Contains(sub_mesh_.bounding_sphere_)) {
+				children_[i]->AddSubmeshToTree(sub_mesh_, index);
+				in_child = true;
+				break;
+			};
+		}
+		if (!in_child) {
+			submesh_indices.push_back(index);
+		}
+		return;
 	}
 };
